3242ff49a2ebd3190607709299b62c4408df6262
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-30 11:07:22 +0300

    completed control flow lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0: 
+        print(counter)
+        counter-=1
+  
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_ints=[num*num for num in int_list]
+    return squared_ints
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range (1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+fizzbuzz() 
